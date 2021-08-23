b371796d9be4f8dc40cd7f65d869482493f3195b
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2021-08-23 00:27:40 -0300

    Fix the var name

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                gtag('config', '${process.env.NEXT_PUBLIC_TRACKING_ID}', {
                   page_path: window.location.pathname,
                 });
               `,
